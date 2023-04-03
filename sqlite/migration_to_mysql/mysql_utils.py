ab1bf7d7d808928cb55b8c5ebc6251d320487e97
benkaan001 <benkaan001@gmail.com>
2023-04-03 01:10:46 -0500

    add get employees

--- sqlite/migration_to_mysql/mysql_utils.py
+++ sqlite/migration_to_mysql/mysql_utils.py
@@ -35,3 +35,17 @@ def get_sqlite_schema(table_name: str = "employees") -> list:
         cursor.execute(metadata_query)
         schema = cursor.fetchall()
         return schema
+
+
+def get_all_employees():
+    """
+    Fetches all employees data from the SQLite database and returns it as a list of tuples
+
+    Returns:
+    list: A list of tuples representing employee data
+    """
+    with sqlite3.connect('employee_database.db') as sqlite_conn:
+        cursor = sqlite_conn.cursor()
+        cursor.execute("SELECT * FROM employees")
+        data = cursor.fetchall()
+    return data
